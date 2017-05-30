1e39d5935e4037d0420cd22d0e7d2cbe1ada20b2
Mathias Wøbbe <Allmynameswastaken@gmail.com>
2017-05-30 17:55:33 +0200

    Update newsdata.py
    Now uses '.format()' instead of '%'.
    
    I also refactored the connection to the database to improve reusability and standardize it.

--- newsdata.py
+++ newsdata.py
@@ -11,19 +11,27 @@ You can read more about the views used here, in the DB-Views.md file.
 
 DBNAME = "news"
 
+def connect(database_name="news"):
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        c = db.cursor()
+        return db, c
+    except:
+        print "Unable to connect to database"
+
 
 def get_top3_articles():
     """Return articles from the database, most viewed first.
     'v_articleViews' is created in news.sql,
     it fetches how many times any article has been accessed."""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
+    db, c = connect()
     print "\nFetching top 3 articles, most popular first...\n"
     c.execute("""SELECT * FROM v_articleViews
                 ORDER BY "Views" DESC
                 LIMIT 3;""")
     for top3_articles in c:
-        print str('%s %s %s' % (top3_articles[0],
+        print str('{} {} {}'.format(top3_articles[0],
                                 top3_articles[1],
                                 "\n"))
     return
@@ -32,8 +40,7 @@ def get_top3_articles():
 
 def get_top_authors():
     """Returns most popular article authors of all time, most popular first"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
+    db, c = connect()
     print "\nFetching top authors, most popular first...\n"
     c.execute("""SELECT name AS "Authors name",
                 total_views AS "Total number of article views"
@@ -41,7 +48,7 @@ def get_top_authors():
                 WHERE authors.id = v_authorViews.author
                 ORDER BY v_authorViews.total_views DESC;""")
     for top_authors in c:
-        print str('%s %s %s %s' % (top_authors[0],
+        print str('{} {} {} {}'.format(top_authors[0],
                                    "with",
                                    top_authors[1],
                                    "Views\n"))
@@ -51,20 +58,19 @@ def get_top_authors():
 
 def error_requests():
     """Returns which days more than 1% of requests were errors"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
+    db, c = connect()
     print "\nFetching which days more than 1% of requests were errors...\n"
     c.execute("""SELECT bad, to_char(day, 'Mon-MM YYYY')
                  FROM v_errorDays""")
     for which_days in c:
-        print str("There were " + '%s %s %s %s' % (which_days[0],
+        print str("There were " + '{} {} {} {}'.format(which_days[0],
                                                    'bad requests',
                                                    which_days[1],
                                                    "\n"))
     return
     db.close()
 
-
-# get_top3_articles()
-# get_top_authors()
-# error_requests()
+if __name__ == '__main__':
+    get_top3_articles()
+    get_top_authors()
+    error_requests()
